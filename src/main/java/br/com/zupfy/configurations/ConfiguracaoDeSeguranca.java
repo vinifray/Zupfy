4466844ab02f27e63c77633e6d451e794b0c145e
Vinícius <vinicius.raymundo@zup.com.br>
2021-04-26 16:16:01 -0300

    Adicionar spring security

---
+++ src/main/java/br/com/zupfy/configurations/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,45 @@
+package br.com.zupfy.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/bandas/**",
+            "/musicas/**",
+            "/albuns/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/bandas/**",
+            "/musicas/**",
+            "/albuns/**"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+
+        http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource configuracaoDeCors(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
